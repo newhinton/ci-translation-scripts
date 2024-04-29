c6921eca4bee122e27eaa0d4e604a10f14ad7bbe
Felix Nüsse <felix.nuesse@t-online.de>
2024-04-29 17:16:26 +0200

    add more logging

--- convertCommitsToTranslations.sh
+++ convertCommitsToTranslations.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-
-touch generatedContent.txt
+TARGET="generatedContent.txt"
+touch "$TARGET"
 
 for file in ${@:2}; do
     if [[ "$file" == "fastlane/metadata/android/"* ]]; then
         echo "$file was changed"
-        cat "$file" >> generatedContent.txt
+        cat "$file" >> "$TARGET"
+	echo "\n" >> "$TARGET"
     fi
 done
-cat generatedContent.txt
+cat "$TARGET"
 
 SEARCH='strings.xml'
 CHANGEDFILES=''
@@ -21,7 +22,7 @@ for file in ${@:2}; do
 done
 
 echo "$CHANGEDFILES"
-
+echo "$1"
 
 DIFF=$(git diff -U0 HEAD~$1 $CHANGEDFILES | grep -E "^\+" | grep -v +++ | cut -c 2- | sed 's/^[ \t]*\(.*$\)/\1/')
 echo "<xml>$DIFF</xml>" | xmlstarlet sel -t -m '//string' -v . -n > changed_texts.txt
@@ -30,4 +31,4 @@ TRANSLATIONS="${TRANSLATIONS//'%'/' ; '}"
 TRANSLATIONS="${TRANSLATIONS//$'\n'/' ; '}"
 TRANSLATIONS="${TRANSLATIONS//$'\r'/' ; '}"
 echo "$TRANSLATIONS"
-echo "$TRANSLATIONS" >> generatedContent.txt
+echo "$TRANSLATIONS" >> "$TARGET"
