e2df2cce1882b53f762cbcf16e98891bcf4dc351
Felix Nüsse <felix.nuesse@t-online.de>
2024-04-29 17:10:19 +0200

    add unified script

---
+++ convertCommitsToTranslations.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+touch generatedContent.txt
+
+for file in ${@:2}; do
+    if [[ "$file" == "fastlane/metadata/android/"* ]]; then
+        echo "$file was changed"
+        cat "$file" >> generatedContent.txt
+    fi
+done
+
+SEARCH='strings.xml'
+CHANGEDFILES=''
+for file in ${@:2}; do
+        if [[ "$file" == *"strings.xml" ]]; then
+              echo "$file was changed"
+              CHANGEDFILES="${CHANGEDFILES} $file"
+        fi
+done
+
+echo "$CHANGEDFILES"
+
+
+DIFF=$(git diff -U0 HEAD~$1 $CHANGEDFILES | grep -E "^\+" | grep -v +++ | cut -c 2- | sed 's/^[ \t]*\(.*$\)/\1/')
+echo "<xml>$DIFF</xml>" | xmlstarlet sel -t -m '//string' -v . -n > changed_texts.txt
+TRANSLATIONS=$(cat changed_texts.txt)
+TRANSLATIONS="${TRANSLATIONS//'%'/' ; '}"
+TRANSLATIONS="${TRANSLATIONS//$'\n'/' ; '}"
+TRANSLATIONS="${TRANSLATIONS//$'\r'/' ; '}"
+echo "$TRANSLATIONS"
+echo "$TRANSLATIONS" >> generatedContent.txt
