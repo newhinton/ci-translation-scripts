f6ee8190cbec2e961537342e373a4a416728550e
Felix Nüsse <felix.nuesse@t-online.de>
2024-04-29 16:32:50 +0200

    fix metadata

--- preprocessMetadata.sh
+++ preprocessMetadata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for file in $@; do
-        if [[ "$file" == "fastlane/metadata/android/**/.txt" ]]; then
-		echo "$file was changed"
-		cat "$file" >> generatedContent.txt
-        fi
+    if [[ "$file" == "fastlane/metadata/android/"* ]]; then
+        echo "$file was changed"
+        cat "$file" >> generatedContent.txt
+    fi
 done
